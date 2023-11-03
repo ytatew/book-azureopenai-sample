ebf62b8f533c54581c4e79248e5210c24ad6bd25
ytatew <qqss28xd@icloud.com>
2023-11-03 15:52:58 +0900

    updates

---
+++ src/ch5/command/openai-api-call.ps1
@@ -0,0 +1,8 @@
+Python src/ch5/code/openai-api-call.py `
+--tenant_id <TENANT_ID> `
+--client_id <CLIENT_ID> `
+--client_secret <CLIENT_SECRET> `
+--redirect_uri <REDIRECT_URI> `
+--scope <SCOPE> `
+--endpoint <ENDPOINT> `
+--subscription_key <SUBSCRIPTION_KEY>
